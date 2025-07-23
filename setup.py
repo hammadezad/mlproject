793b24440769d19c7d2ce9b9bc4ff123172b0d91
hammadezad <hammadezad6@gmail.com>
2025-07-23 21:52:54 +0500

    revised files

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
-def get_requirements(file_path:str) -> List[str]:
-    '''
-    this function will return the list of requirements
-    '''
+HYPHEN_DOT = "-e ."
+
+def get_requirements(file_path:str)-> List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", " ") for req in requirements]
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-    return requirements 
+        requirements = [req.strip() for req in requirements]
 
+        if HYPHEN_DOT in requirements:
+            requirements.remove(HYPHEN_DOT)
+    return requirements
 
 setup(
-name = 'mlproject',
-version = '0.0.1',
-author ='hammadezad',
-author_email = 'hammadezad2211@gmail.com',
-packages = find_packages(), 
-install_requires = get_requirements('requirements.txt')
+    name='mlproject',
+    version='0.0.1',
+    description= "General ML project for any project package",
+    author='hammad_ezad',
+    author_email='hammadezad@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
