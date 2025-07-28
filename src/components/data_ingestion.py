640c7d5f2cd277a11611676e225c440dba27f1dc
hammadezad <hammadezad6@gmail.com>
2025-07-28 16:48:30 +0500

    data_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,9 +9,10 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig():
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
 
 class DataIngestion:
     def __init__(self):
@@ -20,8 +21,32 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv("stud.csv")
+            df = pd.read_csv("notebook/data/stud.csv")
             logging.info("Read the data as Data frame from the dataset")
 
-        except as Exception e:
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.3, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path, 
+                self.ingestion_config.raw_data_path
+            )
+
+
+        except Exception as e:
             raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj= DataIngestion()
+    obj.initiate_data_ingestion()
+
+
